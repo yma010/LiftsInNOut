b233d98a83dd0e3efb2bc4a468220aa21d004ea3
Marvin Ma <yma010@ucr.edu>
2019-10-07 17:13:11 -0700

    initial commit

--- frontend/lifts_in_n_out.jsx
+++ frontend/lifts_in_n_out.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-debugger;
+
 document.addEventListener("DOMContentLoaded", () =>{
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Hello World</h1>, root);
